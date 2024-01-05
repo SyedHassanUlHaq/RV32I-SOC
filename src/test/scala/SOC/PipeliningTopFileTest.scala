3308ff9f2429785941145fd8410ac267cd659277
SyedHassanUlHaq <21b-029-cs@students.uit.edu>
2024-01-05 23:42:32 +0500

    changed package name and removed unnecessary files

---
+++ src/test/scala/SOC/PipeliningTopFileTest.scala
@@ -0,0 +1,14 @@
+package SOC
+
+import chisel3._
+import org.scalatest._
+import chiseltest._
+
+class Topfile_Test extends FreeSpec with ChiselScalatestTester{
+    "Top Test" in {
+        test(new Top){
+            C =>
+            C.clock.step(100)
+        }
+    }
+}
